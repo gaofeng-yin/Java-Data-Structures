a982623112ee41347f5c2364fbc6ba82302fa773
gaofeng <gaofengyin@hotmail.com>
2021-04-09 17:26:13 +0100

    doubly linked list comments

--- src/doublyLinkedList/DoublyLinkedList.java
+++ src/doublyLinkedList/DoublyLinkedList.java
@@ -2,10 +2,14 @@ package doublyLinkedList;
 
 public class DoublyLinkedList<E> {
 	
+	//nested Node class
 	private static class Node<E>{
 		
+		//element stored in this node
 		private E element;
+		//previous node in the list
 		private Node<E> prev;
+		//subsequent node in the list
 		private Node<E> next;
 	
 		public Node(E e, Node<E> p, Node<E> n) {
@@ -36,32 +40,41 @@ public class DoublyLinkedList<E> {
 		
 	}
 	
+	//header sentinel
 	private Node<E> header;
+	//tailer sentinel
 	private Node<E> tailer;
+	//number of nodes in the list
 	private int size = 0;
 	
+	//constructor: create header, create tailer prev point to header and then header set next to tailer
 	public DoublyLinkedList() {
 		header = new Node<>(null,null,null);
 		tailer = new Node<>(null,header,null);
 		header.setNext(tailer);
 	}
 	
+	//return size of the list
 	public int size() {
 		return size;
 	}
 	
+	//is list empty?
 	public boolean isEmpty() {
 		return size == 0;
 	}
 	
+	//get first element of the list
 	public E first() {
 		return header.getNext().getElement();
 	}
 	
+	//get last element of the list
 	public E last() {
 		return tailer.getPrev().getElement();
 	}
 	
+	//Add element to the linked list in between the given node
 	private void addBetween(E e, Node<E> predecessor, Node<E> successor) {
 		Node<E> newest = new Node<>(e,predecessor,successor);
 		predecessor.setNext(newest);
@@ -69,6 +82,7 @@ public class DoublyLinkedList<E> {
 		size++;
 	}
 	
+	//removes the given node from the list and returns it's element
 	private E remove(Node<E> node) {
 		Node<E> predecessor = node.getPrev();
 		Node<E> successor = node.getNext();
@@ -78,14 +92,17 @@ public class DoublyLinkedList<E> {
 		return node.getElement();
 	}
 	
+	//add element to the front of the list
 	public void addFirst(E e) {
 		addBetween(e, header, header.getNext());
 	}
 	
+	//add element to the end of the list 
 	public void addLast(E e) {
 		addBetween(e, tailer.getPrev(), tailer);
 	}
 	
+	//remove and returns the first element of the list
 	public E removeFirst() {
 		if(isEmpty()) {
 			return null;
@@ -93,6 +110,7 @@ public class DoublyLinkedList<E> {
 		return remove(header.getNext());
 	}
 	
+	//remove and returns the last element of the list
 	public E removeLast() {
 		if(isEmpty()) {
 			return null;
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,5 @@
 package main;
 
-import singlyLinkedList.SinglyLinkedList;
-
 public class Main {
 
 	public static void main(String[] args) {
